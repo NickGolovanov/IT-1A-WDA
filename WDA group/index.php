bbdfb5807fa3cde60b1ae1a648f2b005b8f747a2
StefanCazacu1 <stefan.cazacu@student.nhlstenden.com>
2023-10-27 12:19:16 +0200

    Update index.php

--- WDA group/index.php
+++ WDA group/index.php
@@ -9,16 +9,19 @@
 </head>
 
 <body>
-    <div class="header">
-        <a href="" class="headerText">home</a>
-        <a href="" class="headerText">who we are</a>
-        <a href="" class="headerText">services</a>
-        <a href="" class="headerText">our work</a>
-        <a href="" class="headerText">faq</a>
-        <a href="" class="headerText">testimonials</a>
-        <a href="" class="headerText">contact us</a>
-    </div>
+    <section id="home">
+        <div class="header">
+            <a href="#home" class="headerText">home</a>
+            <a href="#who-we-are" class="headerText">who we are</a>
+            <a href="#Our_services" class="headerText">services</a>
+            <a href="#our_work" class="headerText">our work</a>
+            <a href="#section-faq" class="headerText">faq</a>
+            <a href="#testimonial" class="headerText">testimonials</a>
+            <a href="#contact" class="headerText">contact us</a>
+        </div>
+    </section>
     <div class="gridContainer">
+        
         <div class="column-left bb-lightbluegray-fill"></div>
         <div class="left bb-lightbluegray-fill">
             <img class="logo" src="img/logo.png">
@@ -27,48 +30,47 @@
         <div class="right bb-lightbluegray-fill">
             <img class="drawing" src="img/drawing.svg">
         </div>
+        
         <div class="column-right bb-lightbluegray-fill"></div>
+            <div class="column-left bb-bluegray-fill waw-rowspan"></div>
+            <div class="bb-bluegray-fill" id="waw-c2-r6"></div>
+            <div class="bb-bluegray-fill" id="waw-c2-r7"></div>
+            <h1 class="waw-section-header bb-lightbluegray bb-bluegray-fill" id="who-we-are">who we are</h1>
 
+            <h2 class="waw-header" id="waw-c1-r2">down to earth</h2>
+            <p class="waw-text" id="waw-c1-r3">
+                We love to keep things simple.<br>
+                No big gestures, no theatre or promising utopias.
+            </P>
 
-        <div class="column-left bb-bluegray-fill waw-rowspan"></div>
-        <div class="bb-bluegray-fill" id="waw-c2-r6"></div>
-        <div class="bb-bluegray-fill" id="waw-c2-r7"></div>
-        <h1 class="waw-section-header bb-lightbluegray bb-bluegray-fill">who we are</h1>
-
-        <h2 class="waw-header" id="waw-c1-r2">down to earth</h2>
-        <p class="waw-text" id="waw-c1-r3">
-            We love to keep things simple.<br>
-            No big gestures, no theatre or promising utopias.
-        </P>
-
-        <h2 class="waw-header" id="waw-c1-r4">quality driven</h2>
-        <p class="waw-text" id="waw-c1-r5">
-            Our professionalism doesn't follow hypes: it follows quality.
-            We believe in ever growing and striving for the best.
-        </p>
+            <h2 class="waw-header" id="waw-c1-r4">quality driven</h2>
+            <p class="waw-text" id="waw-c1-r5">
+                Our professionalism doesn't follow hypes: it follows quality.
+                We believe in ever growing and striving for the best.
+            </p>
 
 
 
-        <h2 class="waw-header" id="waw-c1-r6">user friendly</h2>
-        <p class="waw-text" id="waw-c1-r7">
-            With our solution we build bridges between user and systems.
-        </p>
+            <h2 class="waw-header" id="waw-c1-r6">user friendly</h2>
+            <p class="waw-text" id="waw-c1-r7">
+                With our solution we build bridges between user and systems.
+            </p>
 
-        <h2 class="waw-header" id="waw-c2-r2">our vision</h2>
-        <p class="waw-text" id="waw-c2-r3">
-            We envision a world where using softwareS
-            is as easy and naturally as breathing.
-        </p>
+            <h2 class="waw-header" id="waw-c2-r2">our vision</h2>
+            <p class="waw-text" id="waw-c2-r3">
+                We envision a world where using softwareS
+                is as easy and naturally as breathing.
+            </p>
 
-        <h2 class="waw-header" id="waw-c2-r4">our mission</h2>
-        <p class="waw-text" id="waw-c2-r5">
-            Our purpose is to develop software that fit the individual qualities of individual needs.
-            To empowering people & businesses with software solutions.
-        </p>
+            <h2 class="waw-header" id="waw-c2-r4">our mission</h2>
+            <p class="waw-text" id="waw-c2-r5">
+                Our purpose is to develop software that fit the individual qualities of individual needs.
+                To empowering people & businesses with software solutions.
+            </p>
         <div class="column-right bb-bluegray-fill waw-rowspan"></div>
         <div id="Our_services">
-        <h1 class="bb-bluegray">our services</h1>
-        <div class="hscroll">
+            <h1 class="bb-bluegray">our services</h1>
+            <div class="hscroll">
                 <div class="servicecontainer bb-bluegray-fill bb-lightbluegray">
                     <div class="service">
                         <div class="serviceinfo">
@@ -77,20 +79,22 @@
                         </div>
                         <div class="feature">
                             <h4>communication and information</h4>
-                            <p>Display information from applications directly in Circle Workspace.  At a glance, 
-                            see all your unread emails, appointments, news, recently edited documents and more.</p>
+                            <p>Display information from applications directly in Circle Workspace. At a glance,
+                                see all your unread emails, appointments, news, recently edited documents and more.</p>
                         </div>
                         <div class="feature">
                             <h4>document managementtuff</h4>
-                            <p>Bring all documents together in one online workplace. So you can work online and 
-                                offline from any device. The documents app bundles and brings structure to Office 365, 
+                            <p>Bring all documents together in one online workplace. So you can work online and
+                                offline from any device. The documents app bundles and brings structure to Office 365,
                                 the fileserver, OneDrive and SharePoint</p>
                         </div>
                         <div class="feature">
                             <h4>app management</h4>
-                            <p>Create a workplace in minutes. We also want to make work easy for workplace administrators. 
-                                We make it possible to create a role- and conditions-based workplace that combines all your
-                                 current and future technologies within one workplace.
+                            <p>Create a workplace in minutes. We also want to make work easy for workplace
+                                administrators.
+                                We make it possible to create a role- and conditions-based workplace that combines all
+                                your
+                                current and future technologies within one workplace.
                             </p>
                         </div>
                     </div>
@@ -103,49 +107,55 @@
                         </div>
                         <div class="feature">
                             <h4>internal communications</h4>
-                            <p>Keep everyone informed, no matter the change, things change especially in the world today.
-                                 Keeping everyone on the same page is a challenge for global businesses. Circle D.O.T.S. 
-                                 makes it easy to target and distribute news to the right people, analyse the impact and 
-                                 optimize the message. The result? Everyone gets where you're going.</p>
+                            <p>Keep everyone informed, no matter the change, things change especially in the world
+                                today.
+                                Keeping everyone on the same page is a challenge for global businesses. Circle D.O.T.S.
+                                makes it easy to target and distribute news to the right people, analyse the impact and
+                                optimize the message. The result? Everyone gets where you're going.</p>
                         </div>
                         <div class="feature">
                             <h4>knowledge management</h4>
                             <p>When people say “I love my job”, they're not talking about the 7.6 hours per week spent
                                 trying to find information they need to do their work. Circle D.O.T.S. brings all your
-                                isparate resources, applications and knowledge together in one easily structured and 
+                                isparate resources, applications and knowledge together in one easily structured and
                                 searchable home.</p>
                         </div>
                         <div class="feature">
                             <h4>connect and engage</h4>
-                            <p>Connect your colleagues and engage communities. If people don't know each other, they can't 
-                                trust each other. If teams don't know what they're doing, they can't help each other. Circle D.O.T.S.
-                                 connects teams and people in an organic way by letting them form Channels and Pages around projects, 
-                                 interests or social initiatives. No more unfamiliar faces, even if you're fully remote.</p>
+                            <p>Connect your colleagues and engage communities. If people don't know each other, they
+                                can't
+                                trust each other. If teams don't know what they're doing, they can't help each other.
+                                Circle D.O.T.S.
+                                connects teams and people in an organic way by letting them form Channels and Pages
+                                around projects,
+                                interests or social initiatives. No more unfamiliar faces, even if you're fully remote.
+                            </p>
                         </div>
                         <div class="feature">
                             <h4>analyze and optimize</h4>
                             <p>For a better working environment, what can be measured, can be improved. Circle D.O.T.S.
-                                helps you to shift from a “send and forget” to an “analyze and optimize” mindset, making 
-                                it easier to experiment and evaluate in a data-driven way. The secret weapon? The world's 
+                                helps you to shift from a “send and forget” to an “analyze and optimize” mindset, making
+                                it easier to experiment and evaluate in a data-driven way. The secret weapon? The
+                                world's
                                 friendliest Analytics dashboard.
                             </p>
                         </div>
-            </div>
-            </div>
+                    </div>
+                </div>
             </div>
         </div>
-     
-        
-     
 
-    <div class="Our_Work">
 
-         <div class="background">
-         <h1 class="waw-section-header bb-lightbluegray bb-bluegray-fill">Our Work</h1>
 
-            <img class="dash" src="dash1.jpeg" alt="image" height="600" width="1200">
+
+        <div class="Our_Work" id="our_work">
+
+            <div class="background">
+                <h1 class="waw-section-header bb-lightbluegray bb-bluegray-fill">Our Work</h1>
+
+                <img class="dash" src="dash1.jpeg" alt="image" height="600" width="1200">
+            </div>
         </div>
-    </div>
 
         <div class="column-left bb-lightbluegray-fill"></div>
         <section id="section-faq" class="bb-lightbluegray-fill">
@@ -185,54 +195,57 @@
                 $answer = $questionLineExploded[1];
                 echo faqQuestion($question, $answer);
             }
-                ?>
-        
-        
+            ?>
+
+
         </section>
         <div class="column-right bb-lightbluegray-fill"></div>
 
         <div class="tEmptyContainer"></div>
-        <div class="testimonials">
+        <div class="testimonials" id="testimonial">
             <h1 class="waw-section-header bb-lightbluegray bb-bluegray-fill">testimonials</h1>
             <div class="tScroll">
                 <div class="tItem">
                     <div class="tGrid">
                         <p class="tAuthor">Jan dornbvoos</p>
-                        <p class="tReview">lorem ipsum dolor sit amet, consectetur adip ip Lorem ipsum dolor sit amet consectetur, adipisicing eliLorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+                        <p class="tReview">lorem ipsum dolor sit amet, consectetur adip ip Lorem ipsum dolor sit amet
+                            consectetur, adipisicing eliLorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
                     </div>
                 </div>
                 <div class="tItem">
                     <div class="tGrid">
                         <p class="tAuthor">Jan dornbvoos</p>
-                        <p class="tReview">lorem ipsum dolor sit amet, consectetur adip ip Lorem ipsum dolor sit amet consectetur, adipisicing eliLorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+                        <p class="tReview">lorem ipsum dolor sit amet, consectetur adip ip Lorem ipsum dolor sit amet
+                            consectetur, adipisicing eliLorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
                     </div>
                 </div>
                 <div class="tItem">
                     <div class="tGrid">
                         <p class="tAuthor">Jan dornbvoos</p>
-                        <p class="tReview">lorem ipsum dolor sit amet, consectetur adip ip Lorem ipsum dolor sit amet consectetur, adipisicing eliLorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+                        <p class="tReview">lorem ipsum dolor sit amet, consectetur adip ip Lorem ipsum dolor sit amet
+                            consectetur, adipisicing eliLorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
                     </div>
                 </div>
             </div>
         </div>
-        
-        <div class="Contact_form">
+
+        <div class="Contact_form" id="contact">
             <div class="Contact_name">
                 <h1>Contact form</h1>
             </div>
             <div class="Form">
                 <?php
-                  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     $name = filter_input(INPUT_POST, "name");
                     $company = filter_input(INPUT_POST, "company");
                     $service = filter_input(INPUT_POST, "service");
                     $email = filter_input(INPUT_POST, "email");
-                    
+
                     $errorflag = false;
                     $errors = array();
-                
+
                     if (empty($name)) {
-                        $errorflag = true; 
+                        $errorflag = true;
                         array_push($errors, "Please enter your name.");
                     }
                     if (empty($company)) {
@@ -244,14 +257,14 @@
                         array_push($errors, "Please enter the service you're looking for.");
                     }
                     if (empty($email)) {
-                        $errorflag = true; 
+                        $errorflag = true;
                         array_push($errors, "Please enter your email.");
                     }
-                
+
                     if ($errorflag) {
                         echo "<div class='error'>";
                         foreach ($errors as $error) {
-                        echo "<p class='error_message'>$error</p>";
+                            echo "<p class='error_message'>$error</p>";
                         }
                         echo "</div>";
                     } else {
@@ -263,8 +276,7 @@
                             </div>
                         ";
                     }
-                } 
-                else {
+                } else {
                     echo "
                         <form action='' method='post'>
                             Hello, I'm <input required class='bb-lightbluegray-fill bb-bluegray' placeholder='name' type='text' name='name' id='name'>
